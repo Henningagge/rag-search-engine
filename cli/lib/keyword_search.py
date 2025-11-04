1380464d797d066f1092b06997a6cbee61af790a
heagge <henning.agge@gmail.com>
2025-11-04 17:08:53 +0100

    removed punctoation

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,12 +1,22 @@
 from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies
-
+import string
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
     for movie in movies:
-        if query in movie["title"]:
+        processed_title = preprocess_text(movie["title"])
+        processed_querry = preprocess_text(query)
+        if processed_querry in processed_title:
             results.append(movie)
             if len(results) >= limit:
                 break
     return results
+
+
+def preprocess_text(text: str) -> str:
+    text = text.lower()
+    chars = string.punctuation
+    for char in chars:
+        text = text.replace(char, "")
+    return text
