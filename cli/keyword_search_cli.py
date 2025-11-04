3923717182bb59a0d6b340ccebcffedbb5383c7e
heagge <henning.agge@gmail.com>
2025-11-04 21:39:11 +0100

    finaly it works thank good that took 3 hours and 2000 token from Gippity

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,13 +2,31 @@
 
 import argparse
 import json
-from lib.keyword_search import search_command
+from lib.keyword_search import search_command, InvertedIndex 
+
+
+
+def handle_build_command():
+        inverindex =  InvertedIndex()
+    
+        inverindex.build()
+
+        docs = inverindex.get_document("merida")
+        inverindex.save()
+        print(f"First document for token 'merida' = {docs[0]}")
+
+
+
 def main() -> None:
+    
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
+
+    build_parser = subparsers.add_parser("build", help="builds docs")
+    build_parser.set_defaults(func=handle_build_command)
 
     args = parser.parse_args()
 
@@ -20,6 +38,8 @@ def main() -> None:
                 print(f"{i}. {res['title']}")
         case _:
             parser.print_help()
-    
+    handle_build_command()
 if __name__ == "__main__":
     main()
+
+
