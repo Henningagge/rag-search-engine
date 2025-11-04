1aed4bf2dab09eca34a64b4e165def6e11a6decf
heagge <henning.agge@gmail.com>
2025-11-04 16:47:54 +0100

    my implementation of getting the json

---
+++ cli/keyword_search_cli.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Keyword Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    search_parser = subparsers.add_parser("search", help="Search movies using BM25")
+    search_parser.add_argument("query", type=str, help="Search query")
+
+    args = parser.parse_args()
+
+    match args.command:
+        case "search":
+            print(f"Searching for: {args.query}")
+            with open ('data/movies.json', 'r') as file:
+                movies = json.load(file)
+
+            results = []
+            for movie in movies["movies"]:
+                if args.query in movie["title"]:
+                    results.append(movie["title"])
+            for i in range(0, 5):
+                print(f"{i+1}. {results[i]}")
+        case _:
+            parser.print_help()
+    
+if __name__ == "__main__":
+    main()
